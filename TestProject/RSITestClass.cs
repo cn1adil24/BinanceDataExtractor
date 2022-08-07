c86fd581d2a98a831a8885e8cd4e8404286a4137
Shehmeer <shehmeer_magician@hotmail.com>
2022-08-07 16:07:54 +0500

    RSI calculator test class

---
+++ TestProject/RSITestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalAnalysis;
+
+namespace TestProject
+{
+    class RSITestClass
+    {
+        public void TestMethod(double [] points)
+        {
+            RSICalculator calculatorWithSmoothing = new RSICalculator(14, "RSI", true);
+            foreach (var point in points)
+            {
+                calculatorWithSmoothing.AddDataPoint(point);
+                Console.WriteLine($"RSI: {calculatorWithSmoothing.Result}");
+            }
+            
+            RSICalculator calculator = new RSICalculator(14, "RSI", false);
+            foreach (var point in points)
+            {
+                calculator.AddDataPoint(point);
+                Console.WriteLine($"RSI: {calculator.Result}");
+            }
+        }
+    }
+}
