874aea27b466c7cf5e3a296583ed76a849616e39
cn1adil24 <shehmeeradil123@gmail.com>
2023-09-04 21:01:51 +0200

    Add Database project and the entity model

---
+++ DatabaseManager/Data/CandleStickDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DatabaseManager.Models;
+using System.Data.Entity;
+
+namespace DatabaseManager.Data
+{
+    internal class CandleStickDbContext : DbContext
+    {
+        public CandleStickDbContext() : base("BinanceDbConnectionString")
+        {
+        }
+
+        public DbSet<Candlestick> CandleStickData { get; set; }
+    }
+}
---
+++ DatabaseManager/Program.cs
@@ -0,0 +1,23 @@
+﻿using DatabaseManager.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Usage
+            
+            using (var dbContext = new CandleStickDbContext())
+            {
+                var data = dbContext.CandleStickData.ToList();
+            }
+            */
+        }
+    }
+}
