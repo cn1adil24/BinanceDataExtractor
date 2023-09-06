a47177e05ff861faf1b1ea60a910f7daacf887f6
cn1adil24 <shehmeeradil123@gmail.com>
2023-09-06 17:29:51 +0200

    Database writer class added

---
+++ DatabaseManager/DatabaseWriter.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using DatabaseManager.Data;
+using DatabaseManager.Models;
+using System.Collections.Generic;
+
+namespace DatabaseManager
+{
+    public class DatabaseWriter
+    {
+        public void WriteRecord(Dictionary<string, string> record)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+
+            IMapper mapper = config.CreateMapper();
+
+            using (var dbContext = new CandleStickDbContext())
+            {
+                var candleStickRecord = mapper.Map<Dictionary<string, string>, Candlestick>(record);
+                dbContext.CandleStickData.Add(candleStickRecord);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
